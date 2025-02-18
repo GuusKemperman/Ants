c080bb62746018448ed63d1499d7d357aad8415c
GuusKemperman <guus.kemperman@gmail.com>
2025-02-18 20:29:58 +0100

    Spawn pheromones using C++

--- Source/Commands/EmitPheromoneCommand.cpp
+++ Source/Commands/EmitPheromoneCommand.cpp
@@ -3,35 +3,33 @@
 
 #include "Assets/Prefabs/Prefab.h"
 #include "Components/TransformComponent.h"
+#include "Components/Physics2D/DiskColliderComponent.h"
+#include "Components/Physics2D/PhysicsBody2DComponent.h"
 #include "Core/AssetManager.h"
 #include "World/Registry.h"
 #include "World/World.h"
 
 void Ant::EmitPheromoneCommand::Execute(CE::World& world, std::span<const EmitPheromoneCommand> commands)
 {
-	CE::AssetHandle pheromonePrefab = CE::AssetManager::Get().TryGetAsset<CE::Prefab>("PF_Pheromone");
+	CE::Registry& reg = world.GetRegistry();
 
-	if (pheromonePrefab == nullptr)
-	{
-		LOG(LogGame, Error, "No pheromone prefab!");
-		return;
-	}
-
-	auto& pheromoneComponentStorage = world.GetRegistry().Storage<PheromoneComponent>();
+	auto& pheromoneComponentStorage = reg.Storage<PheromoneComponent>();
+	auto& transforms = reg.Storage<CE::TransformComponent>();
+	auto& disks = reg.Storage<CE::DiskColliderComponent>();
+	auto& physics = reg.Storage<CE::PhysicsBody2DComponent>();
 
 	for (const EmitPheromoneCommand& command : commands)
 	{
-		glm::vec3 pos = CE::To3D(command.mLocation);
-		entt::entity createdEntity = world.GetRegistry().CreateFromPrefab(*pheromonePrefab, entt::null, &pos);
+		entt::entity entity = reg.Create();
 
-		if (!pheromoneComponentStorage.contains(createdEntity))
-		{
-			LOG(LogGame, Error, "Pheromone prefab did not spawn entity, or entity did not contain pheromone component");
-			world.GetRegistry().Destroy(createdEntity, true);
-			return;
-		}
+		pheromoneComponentStorage.emplace(entity, command.mPheromoneId);
+		transforms.emplace(entity).SetWorldPosition(command.mLocation);
+		disks.emplace(entity, CE::DiskColliderComponent{ PheromoneComponent::sRadius });
 
-		PheromoneComponent& pheromoneComponent = pheromoneComponentStorage.get(createdEntity);
-		pheromoneComponent.mPheromoneId = command.mPheromoneId;
+		CE::PhysicsBody2DComponent& body = physics.emplace(entity);
+		body.mIsAffectedByForces = false;
+		body.mRules = {};
+		body.mRules.mLayer = CE::CollisionLayer::Query;
+		body.mRules.SetResponse(CE::CollisionLayer::Query, CE::CollisionResponse::Overlap);
 	}
 }
